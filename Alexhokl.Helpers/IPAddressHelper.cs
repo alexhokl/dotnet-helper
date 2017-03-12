ac06dbc6a8553a822a1549279dd509e9e0c39438
alexhokl <alexhokl@gmail.com>
2017-03-12 10:15:13 +0800

    Added IPAddressHelper

---
+++ Alexhokl.Helpers/IPAddressHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alexhokl.Helpers
+{
+    public static class IPAddressHelper
+    {
+        /// <summary>
+        /// Gets the network address.
+        /// </summary>
+        /// <param name="address">The address.</param>
+        /// <param name="subnetMask">The subnet mask.</param>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidOperationException">Length of specified address and subnet mask does not match.</exception>
+        public static IPAddress GetNetworkAddress(IPAddress address, IPAddress subnetMask)
+        {
+            byte[] addressBytes = address.GetAddressBytes();
+            byte[] maskBytes = subnetMask.GetAddressBytes();
+
+            if (addressBytes.Length != maskBytes.Length)
+                throw new InvalidOperationException(
+                    "Length of specified address and subnet mask does not match.");
+
+            byte[] networkAddressBytes = new byte[addressBytes.Length];
+            Parallel.For(
+                0, networkAddressBytes.Length, i => 
+                    networkAddressBytes[i] = (byte)(addressBytes[i] & maskBytes[i]));
+            return new IPAddress(networkAddressBytes);
+        }
+    }
+}
